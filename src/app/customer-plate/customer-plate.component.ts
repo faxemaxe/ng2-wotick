893eecf400650b1500fa54c36d2d4ecb934ab38c
Max Hoeffner <maxhoeffner@aol.com>
2017-02-25 18:33:08 +0100

    add cardUI for customer view // only CRUD operation missing is create

--- src/app/customer-plate/customer-plate.component.ts
+++ src/app/customer-plate/customer-plate.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {Customer} from "../types/customer.type";
+import {CustomerService} from "../customer.service";
 
 @Component({
   selector: 'app-customer-plate',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-plate.component.css']
 })
 export class CustomerPlateComponent implements OnInit {
+  editable: boolean;
+  _customer: any;
 
-  constructor() { }
+  constructor(private customerService: CustomerService) {
+    this.editable = false;
+    this._customer = {}
+  }
+
+  @Input() customer: Customer;
 
   ngOnInit() {
   }
 
+  startEdit() {
+    Object.assign(this._customer, this.customer);
+    this.editable = true;
+  }
+
+  saveEdit() {
+    this.customerService.update(this.customer);
+    this.editable = false;
+  }
+
+  cancelEdit() {
+    Object.assign(this.customer, this._customer);
+    this.editable = false;
+  }
+
+  deleteCustomer() {
+    this.customerService.remove(this.customer.customerUUID);
+    this.editable = false;
+  }
 }
